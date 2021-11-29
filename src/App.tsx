d4ffd1a20ed86f5d83fb45fd0e55340026728bc9
WAPLAY <tkdgns1341@gmail.com>
2021-11-30 01:51:58 +0900

    Body 헤더 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React from "react";
+import styled from "styled-components";
 import reset from "styled-reset";
 
 import NavBar from "./components/Nav/NavBar";
+
+const Body = styled.div`
+  display: flex;
+`;
 function App() {
   return (
     <>
-      <NavBar />
+      <Body>
+        <NavBar />
+      </Body>
     </>
   );
 }
