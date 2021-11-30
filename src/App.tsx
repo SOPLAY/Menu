bc8d01bc82f54b4cce144be802bf94ae0419f950
WAPLAY <tkdgns1341@gmail.com>
2021-12-01 02:15:07 +0900

    logo 정리 및 nav바 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,33 @@
+import { Checkbox, Image } from "antd";
+import { CheckboxValueType } from "antd/lib/checkbox/Group";
+import { Header } from "antd/lib/layout/layout";
+import "antd/dist/antd.css";
 import React from "react";
 import styled from "styled-components";
-import reset from "styled-reset";
-
-import NavBar from "./components/Nav/NavBar";
 
 const Body = styled.div`
   display: flex;
 `;
+function onChange(checkedValues: CheckboxValueType[]) {
+  console.log("checked = ", checkedValues.join(" "));
+}
+
+const options = [
+  { label: "메인메뉴", value: "mainMenu" },
+  { label: "반찬", value: "subMenu" },
+];
 function App() {
   return (
     <>
       <Body>
-        <NavBar />
+        <Header>
+          <img src="./img/h_logo.png" alt="a" />
+        </Header>
+        <Checkbox.Group
+          options={options}
+          defaultValue={["메인메뉴"]}
+          onChange={onChange}
+        />
       </Body>
     </>
   );
